cd1a2338fdfcc8e62ef291d56fb395a4ae9d1bdd
GABRIEL GARCIA H. <gagarcia@panama-tec.com>
2024-11-23 15:30:16 -0500

    vista finalizada, inicio de integracin front back

--- assets/main.js
+++ assets/main.js
@@ -1,162 +1,28 @@
 import { toggle_element, redirigir, HtmlEncode } from "./modules/funcionesGenerales/index.js";
 import { request } from "./modules/request/index.js";
+import { detallasGenerales } from "./modules/detallesGenerales/index.js";
 
-import { mostrar_detalles_libros } from "./modules/detallesGenerales/index.js";
-import { libros } from "./modules/libros/index.js";
+import { Receta } from "./modules/Recetas/index.js";
+
+var arreglo_recetas = "";
+var arreglo_recetas_detalle = "";
 
 const urlParams = new URLSearchParams(window.location.search);
 
 // VALORES POR DEFECTO DEL API DE GOOGLE BOOKS.
 const GOOGLE_BOOK_URL = 'http://localhost/PARCIALES/PARCIAL_4/assets/php/GoogleBooks/google_book.php';
-const cantidad_libros = 40;
+const cantidad_receta = 40;
 const libro_buscado = "php";
 
 
-var libros_disponibles = "";
-var arreglo_libros = "";
-var arreglo_libros_detalle = "";
+window.addEventListener('load', () => {
 
-
-
-const toggle_detalle = (titulo_libro, descripcion, imagen, autor, ano_publicacion, my_book_id, resena_personal) => {  
-
-    if(my_book_id){
-
-        detalle_libro.innerHTML = "";
-        detalle_libro.innerHTML = mostrar_detalles_libros(titulo_libro, descripcion, imagen, autor, ano_publicacion, my_book_id, resena_personal);
-
-    }
-
-    toggle_element(detalle_libro);
-}
-
-const obtener_libro = (imagen_libro, titulo_libro, descripcion, autor, ano_publicacion, my_book_id, resena_personal) => {
-
-    if(imagen_libro && titulo_libro) {
-        // console.log(imagen_libro.thumbnail);
-       return libros(imagen_libro, titulo_libro, descripcion, autor, ano_publicacion, my_book_id, resena_personal);
-    }
-
-}
-
-const agregar_favorito = (titulo_libro, descripcion, imagen, autor, ano_publicacion, my_book_id) => {
-    
-    let api = 'http://localhost/PARCIALES/PARCIAL_4/assets/php/guardar_favoritos.php';
-    
-    let my_resena_personal = document.getElementById('text_area_detalle');
-    
-    let my_resena_personal_value = my_resena_personal.value ? my_resena_personal.value : 'Sin resena';
-    
-    if (my_book_id && titulo_libro && descripcion && imagen && autor){
-        
-        const URL = `${api}?user_id=${encodeURIComponent('1')}&google_books_id=${encodeURIComponent(my_book_id)}&titulo=${encodeURIComponent(titulo_libro)}&autor=${encodeURIComponent(autor)}&imagen_portada=${encodeURIComponent(imagen)}&resena_personal=${encodeURIComponent(my_resena_personal_value)}&descripcion_libro=${encodeURIComponent(descripcion)}`;
-        const mensaje = request(URL);    
-    
-        if(mensaje.mensaje == "00x1"){
-
-            alert('Libro no agregado hubo error en el usuario.');
-
-        }else{
-
-            alert('Libro agregado a la base de datos.');
-
-        }
-        
-        toggle_detalle();
-        
-    }else {
-        alert('error_detectado');
-    }
-    
-}
-
-const eliminar_favoritos = (book_id) => {
-    
-    let api = 'http://localhost/PARCIALES/PARCIAL_4/assets/php/eliminar_favorito.php';
-
-    if (book_id){
-
-        detalle_libro.innerHTML = "";
-        contenedor_libro.innerHTML = '';    
-        const URL = `${api}?google_books_id=${encodeURIComponent(book_id)}`;
-        
-        request(URL);
-        alert('Libro eliminado a la base de datos.');
-        toggle_detalle();
-        reload_favoritos();
-        
-    }else {
-        alert('error_detectado');
-    }
-
-}
-
-
-const reload_favoritos = async () => {
-    contenedor_libro.innerHTML = '';
-    contenedor_libro.innerHTML = "Pendiente Lista favoritos";
-    let HTML = "";
-    let id = 0;
-
-    arreglo_libros_detalle = await request(`http://localhost/PARCIALES/PARCIAL_4/assets/php/lista_favoritos.php?user_id=1`);
-    
-    arreglo_libros_detalle.forEach(element => {
-        // console.log(element); 
-        HTML += obtener_libro( HtmlEncode(element.imagen_portada), HtmlEncode( element.titulo),  HtmlEncode( element.descripcion),  HtmlEncode( element.autor), "ND",  HtmlEncode( element.google_book_id),  HtmlEncode( element.resena_personal));
-        id++
-    })
-
-    contenedor_libro.innerHTML = "";
-    contenedor_libro.innerHTML = HTML;
-
-}
-
-
-const listar_libros = (array) => {
-
-    let HTML = "";
-    let id = 0;
-
-    array.forEach(element => {
-        let imagen_libro = "";
-        let titulo_libro = "";
-        let descripcion = "";
-        let autor = "";
-        let ano_publicacion = "";
-        let my_book_id = "";
-        
-        try{
-
-            imagen_libro =  HtmlEncode(element.volumeInfo.imageLinks.thumbnail);
-            titulo_libro =  HtmlEncode(element.volumeInfo.title);
-            descripcion = HtmlEncode(element.volumeInfo.description);
-    
-            autor =  typeof element.volumeInfo.authors[0] !== undefined  ? HtmlEncode(element.volumeInfo.authors[0]) :" ND";
-            ano_publicacion =  HtmlEncode(element.volumeInfo.publishedDate);
-            my_book_id =  HtmlEncode(element.id);
-
-        }catch(error){
-            console.log(error);
-        }
-
-        if(imagen_libro && titulo_libro) {
-            // console.log(imagen_libro.thumbnail);
-            HTML += libros(imagen_libro, titulo_libro, descripcion, autor, ano_publicacion, my_book_id);
-        }
-
-        
-
-        id += 1;
-            
-    });
-
-    return HTML;
-}
+});
 
 
 
 window.toggle_detalle = toggle_detalle;
-window.arreglo_libros = arreglo_libros;
+window.arreglo_receta = arreglo_receta;
 window.redirigir = redirigir;
 window.agregar_favorito = agregar_favorito;
 window.eliminar_favoritos = eliminar_favoritos;
--- assets/modules/Recetas/index.js
+++ assets/modules/Recetas/index.js
@@ -1,7 +1,85 @@
-export const libros = (imagen, titulo_libro, descripcion, autor, ano_publicacion, my_book_id, resena_personal) => {
-    
+export const Receta = (OBJECT_RECETA) => {
+
     return `
-       
-    
+        <div class="componente_receta row">
+
+            <img class="componente_recetas_imagen" src='.\assets\images\receta_presentacion.png' class="shadow" height="100%">
+
+            <div class="componente_receta_contenedor_der col" style="position: relative;">
+
+                <div class="componente_receta_contenedor_der_sup col">
+
+                    <label class="componente_receta_titulo row">
+
+                        <b>${titulo_receta}</b>
+
+                    </label>
+
+                    <div class="componente_receta_descripcion">
+                        ${descripcion}
+
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam rem numquam, placeat repellat delectus
+                        cumque sed quidem. Incidunt, provident possimus error magnam eligendi totam id repudiandae ratione
+                        quisquam
+                        libero non. Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt nesciunt facere quo
+                        temporibus
+                        sapiente quasi rerum odit nobis accusamus dicta laudantium veniam distinctio iste, inventore voluptates
+                        error incidunt! Harum, sapiente.
+
+                    </div>
+
+                </div>
+
+                <div class="componente_receta_contenedor_der_inf row">
+
+                    <div class="detalle_receta_detalles_informacion_autor"><b>Autor del receta:</b> ${autor} </div>
+
+                    <div class="componente_receta_stars">
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                            class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                            class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                            class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                            class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                            class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                    </div>
+
+                </div>
+
+                <div id='' class="detalle_receta_close_tag" onclick='toggle_element("detalle")'>
+                    <svg class="clickeable" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white"
+                        class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+                    </svg>
+                </div>
+
+            </div>
+
+        </div>
+
     `;
-} 
+}
